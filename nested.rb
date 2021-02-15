d398867050c2486fd256e99bd666a225b3027fa0
Colten Appleby <colten96@gmail.com>
2021-02-15 04:53:25 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -82,6 +82,7 @@ def adding_matz
       }
     }
   programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]}
+  return programmer_hash
 end
 
 def changing_alan
@@ -104,6 +105,7 @@ def changing_alan
       }
     }
   programmer_hash[:alan_kay][:known_for] = "GUI"
+  return programmer_hash
 end
 
 def adding_to_dennis
@@ -126,4 +128,5 @@ def adding_to_dennis
       }
     }
   programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  return programmer_hash
 end
